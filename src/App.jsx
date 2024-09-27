be7020629b26c1796192a059276ff99a910b0e53
daniel <diordanny4@gmial.com>
2024-09-27 12:07:26 +0100

    mistake at a nabar function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar/navbar'
+import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/title/title'
